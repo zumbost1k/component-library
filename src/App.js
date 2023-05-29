e5740791053884925d4ce24b5e44d3467c1a6f7c
zumbost1k <matusevich.mi@gmail.com>
2023-05-29 17:48:54 +0300

    add deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import Buttons from "./buttons/buttons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Buttons color={"secondary"} size={"small"} icon={"right_arrow"} disabled={true} />
+      <br />
+      <Buttons color={"primary"} size={"block"} icon={"center_chat"} disabled={true} />
+      <br />
+      <Buttons color={"outline"} size={"large"} icon={"left_chat"} disabled={true} />
+      <br />
+      <Buttons color={"transparent"} size={"small"} icon={"left_chat"} disabled={true} />
     </div>
   );
 }
---
+++ src/buttons/buttons.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import './buttons.css'
+
+const iconTypeCheker = (icon, color, size) => {
+    if (icon === 'left_chat' && color === 'primary') {
+        if (size !== "small") { return 'left_chat_primary' }
+        else { return 'left_chat_primary_small' }
+
+    }
+    if (icon === 'right_arrow' && color === 'primary') {
+        if (size !== "small") { return 'right_arrow_primary' }
+        else { return 'right_arrow_primary_small' }
+    }
+    if (icon === 'center_chat' && color === 'primary') {
+        if (size !== "small") { return 'center_chat_primary' }
+        else { return 'center_chat_primary_small' }
+    }
+    if (icon === 'left_chat') {
+        if (size !== "small") { return 'left_chat' }
+        else { return 'left_chat_small' }
+    }
+    if (icon === 'right_arrow') {
+        if (size !== "small") { return 'right_arrow' }
+        else { return 'right_arrow_small' }
+    }
+    if (icon === 'center_chat') {
+        if (size !== "small") { return 'center_chat' }
+        else { return 'center_chat_small' }
+    }
+    else { return '' }
+}
+
+
+const Buttons = (props) => {
+    const size = props.icon && props.icon !== `right_arrow` && props.size !== 'block' ? `${props.size}_with_icon_left` : props.icon && props.size !== 'block' ? `${props.size}_with_icon_right` : props.size
+    const className = `${props.color} ${size} ${iconTypeCheker(props.icon, props.color, props.size)}`
+    return (
+        <div><button className={className} >{`${props.size} + ${props.color}`}</button></div>
+    )
+
+}
+export default Buttons
