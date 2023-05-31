1117f80a6cb331053e85a12438f10596ef14c226
zumbost1k <matusevich.mi@gmail.com>
2023-05-31 20:09:26 +0300

    add dinamic props changer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-
-import React from "react";
-import Buttons from "./button/button";
+import React, { useState } from "react";
+import Button from "./button/button";
 
 function App() {
+  const [size, setSize] = useState("block")
+  const [color, setColor] = useState("primary")
+  const [icon, setIcon] = useState("")
+  const [disabled, setDisabled] = useState(false)
   return (
     <div >
-      <Buttons color={"secondary"} size={"small"} icon={"right_arrow"} disabled={true} />
+      <Button color={color} size={size} icon={icon} disabled={disabled} />
       <br />
-      <Buttons color={"primary"} size={"block"} icon={"center_chat"} disabled={true} />
-      <br />
-      <Buttons color={"outline"} size={"large"} icon={"left_chat"} disabled={true} />
-      <br />
-      <Buttons color={"transparent"} size={"small"} icon={"left_chat"} disabled={true} />
+      <form className="myForm">
+        <select onChange={newSize => { setSize(newSize.target.value) }}>
+          <option value="block">block</option>
+          <option value="large">large</option>
+          <option value="small">small</option>
+        </select>
+        <select onChange={newIcon => { setIcon(newIcon.target.value) }}>
+          <option value="">no icon</option>
+          <option value="left_chat">left</option>
+          <option value="center_chat">center</option>
+          <option value="right_arrow">right</option>
+        </select>
+        <select onChange={newColor => { setColor(newColor.target.value) }}>
+          <option value="primary">primary</option>
+          <option value="secondary">secondary</option>
+          <option value="outline">outline</option>
+          <option value="transparent">transparent</option>
+        </select>
+        <select onChange={newDisabled => { setDisabled(newDisabled.target.value) }}>
+          <option value={false}>no disable</option>
+          <option value={true}>disable</option>
+        </select>
+      </form>
     </div>
+
   );
 }
 
--- src/button/button.jsx
+++ src/button/button.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './button.css'
-
+import arrow from "../icons/arrow.svg"
+import chat from "../icons/chat.svg"
 const iconTypeCheker = (icon, color, size) => {
     if (icon === 'left_chat' && color === 'primary') {
         if (size !== "small") { return 'left_chat_primary' }
@@ -28,14 +29,23 @@ const iconTypeCheker = (icon, color, size) => {
     }
     else { return '' }
 }
-
-
-const Buttons = (props) => {
+const svgChecker = (icon, size, color) => {
+    const iconType = (icon === 'center_chat' || icon === 'left_chat' || icon === 'right_arrow') ? (icon === 'center_chat' || icon === 'left_chat') && size !== 'small' ? chat : size === 'small' && icon === 'center_chat' ? chat : arrow : ""
+    if (iconType !== '') {
+        return <img src={iconType} alt="logo" className={iconTypeCheker(icon, color, size)} />
+    }
+    else { return '' }
+}
+const Button = (props) => {
+    const isDisable = props.disabled === 'true' ? true : false
     const size = props.icon && props.icon !== `right_arrow` && props.size !== 'block' ? `${props.size}_with_icon_left` : props.icon && props.size !== 'block' ? `${props.size}_with_icon_right` : props.size
-    const className = `${props.color} ${size} ${iconTypeCheker(props.icon, props.color, props.size)}`
+    const className = `${props.color} ${size} `
+
     return (
-        <div><button className={className} >{`${props.size} + ${props.color}`}</button></div>
+        <div>
+            <button className={className} disabled={isDisable}>{svgChecker(props.icon, props.size, props.color)}{`${props.size} + ${props.color}`}</button>
+        </div>
     )
 
 }
-export default Buttons
+export default Button
