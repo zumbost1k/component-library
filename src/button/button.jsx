39736a106bec0b85ab64d009fb5a31d402befde1
zumbost1k <matusevich.mi@gmail.com>
2023-06-07 13:35:54 +0300

    add class name filter

--- src/button/button.jsx
+++ src/button/button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './button.css'
 
 const Button = ({ color, size, IconClass, type, disabled, iconType }) => {
-    const className = `button ${color} ${size} `
+    const className = ['button',color,size].filter(Boolean).join(' ')
     return (
         <div >
             <button className={className} disabled={disabled}>
