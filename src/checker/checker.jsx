1be3f918255cd1b681c819375316baae334e6bd4
zumbost1k <matusevich.mi@gmail.com>
2023-06-23 19:11:14 +0300

    final checkBox

--- src/checker/checker.jsx
+++ src/checker/checker.jsx
@@ -3,15 +3,13 @@ import './checker.css'
 
 const Checker = ({ isDisabled, checked, onChange }) => {
     return (
-        <div>
+        
             <div className='cheker'>
                 <div>
                     <input onChange={e => { onChange(!checked) }} className={checked ? 'checked' : 'custom-checkbox'} type='checkbox' id='checkbox1' name='contact' disabled={isDisabled} />
                     <label for='checkbox1'></label>
                 </div>
             </div>
-
-        </div>
     )
 }
 
